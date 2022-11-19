4f60aac8d163d46f239b31f31c6f2ea082ce9966
gaston henrich <henrichgaston@gmail.com>
2022-11-19 17:48:16 -0300

    view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,16 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+def new
+  @task = Task.new
+end
+def create
+  @task = Task.new(task_params)
+  @task.save
+  redirect_to task_path
+end
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show", as:  :task
+  get "tasks/new", to: "tasks#new",  as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create", as: :create
 
 end
